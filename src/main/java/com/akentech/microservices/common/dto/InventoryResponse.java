b53f88d32044045870ebdf86b59dc92146271ae1
kenrique100 <ngwakenri2016@gmail.com>
2025-02-05 16:46:41 -0500

    inventory response share library for order

---
+++ src/main/java/com/akentech/microservices/common/dto/InventoryResponse.java
@@ -0,0 +1,13 @@
+package com.akentech.microservices.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class InventoryResponse {
+    private String skuCode;
+    private boolean isInStock;
+}
